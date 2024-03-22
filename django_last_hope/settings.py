a2a10aac7cc4a0a1cbb7d0aa47af01612a3a9b19
TKDKB <63513743+TKDKB@users.noreply.github.com>
2024-03-22 20:55:52 +0300

    update

--- django_last_hope/settings.py
+++ django_last_hope/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DATABASE_NAME"),
-        "USER": os.environ.get("USER"),
+        "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
         "HOST": os.environ.get("DATABASE_HOST"),  # IP адрес или домен СУБД.
         "PORT": 5432,
